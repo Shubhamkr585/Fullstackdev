b24ad443c7cbdfb0e4613466a147976a4652d930
Shubhamkr585 <shubhamprd585@gmail.com>
2025-03-21 02:57:23 +0530

    utility added

---
+++ src/utils/ApiError.js
@@ -0,0 +1,19 @@
+class AppiError extends Error {
+    constructor(status, message="something went wrong",errors=[],stack="") {
+        super(message);
+        this.status = status;
+        this.errors = errors;
+        this.stack = stack
+        this.data=null   
+
+        if(stack){
+            this.stack=stack
+        }
+        else{
+           Error.captureStackTrace(this,this.constructor)
+
+        }
+    }
+}
+
+export  {AppiError}
