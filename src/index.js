878cd85a792170a8ecf6805338db59df102e5e3c
Shubhamkr585 <shubhamprd585@gmail.com>
2025-03-21 02:41:54 +0530

    middleware added

--- src/index.js
+++ src/index.js
@@ -4,14 +4,24 @@
 import dotenv from "dotenv"
 import  connectDB  from "./db/index.js";
  
-
+import app from "./app.js"
 
 
 dotenv.config({
     path:"./.env"
 })
 
-connectDB()
+connectDB().
+then(()=>{
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log(`The app is running fast on the port ${process.env.PORT || 3000}`)
+    })
+}).
+catch((error)=>{
+    console.error("MONGODB CONNECTION FAILED ",error);
+    process.exit(1);
+    throw error
+})
 
 
 
